41bdf8bfa9f9c4d2a908c9069971ffd405db8a5f
Draldo <draldo_lopez@hotmail.com>
2016-07-29 16:59:26 -0400

    async example

--- app/src/main/java/com/example/admin/asynctaskexample/MainActivity.java
+++ app/src/main/java/com/example/admin/asynctaskexample/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.admin.asynctaskexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTAG_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new ExampleTask().execute();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    public void doMagic(View view) {
+        Log.d(TAG, "doMagic: HelloWorld");
     }
 }
